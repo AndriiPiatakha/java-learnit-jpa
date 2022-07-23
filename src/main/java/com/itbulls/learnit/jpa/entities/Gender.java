000b6e043c833b3023e64ca019920067871b6920
Andrey Pyatakha <andrey.pyatakha@gmail.com>
2022-07-24 01:07:03 +0300

    init commit

---
+++ src/main/java/com/itbulls/learnit/jpa/entities/Gender.java
@@ -0,0 +1,6 @@
+package com.itbulls.learnit.jpa.entities;
+
+public enum Gender {
+    MALE, 
+    FEMALE
+}
---
+++ src/main/java/com/itbulls/learnit/jpa/entities/ProjectId.java
@@ -0,0 +1,20 @@
+package com.itbulls.learnit.jpa.entities;
+
+public class ProjectId {
+	private int departmentId;
+	private long projectId;
+	public int getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+	public long getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(long projectId) {
+		this.projectId = projectId;
+	}
+	
+	
+}
