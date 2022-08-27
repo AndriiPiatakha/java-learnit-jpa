8e9b14528f57a6d1f829fb8d860aa8362b34b017
Andrey Pyatakha <andrey.pyatakha@gmail.com>
2022-08-28 01:20:10 +0300

    N+1 problem and solution

---
+++ src/main/java/com/itbulls/learnit/jpa/n1/solution/Category.java
@@ -0,0 +1,68 @@
+package com.itbulls.learnit.jpa.n1.solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "category_name")
+	private String categoryName;
+	
+	@Column(name = "is_available")
+	private boolean isAvailable;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+	private List<Product> products = new ArrayList<>();
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", categoryName=" + categoryName + ", isAvailable=" + isAvailable + ", products="
+				+ products + "]";
+	}
+	
+}
---
+++ src/main/java/com/itbulls/learnit/jpa/n1/solution/Product.java
@@ -0,0 +1,54 @@
+package com.itbulls.learnit.jpa.n1.solution;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+ 
+    @Column(name = "product_name")
+    private String productName;
+    
+    @ManyToOne
+    private Category category;
+    
+    public Product() {
+	}
+    
+    public Product(String productName, Category category) {
+    	this.productName = productName;
+    	this.category = category;
+    }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+}
